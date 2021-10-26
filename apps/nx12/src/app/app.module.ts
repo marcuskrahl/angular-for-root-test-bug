c1488f8f1e8a80f75ea9036fcd3b45c185f4f9d3
Marcus Krahl <Marcus.Krahl@t-systems.com>
2021-10-26 09:40:50 +0200

    add remaining files

--- apps/nx12/src/app/app.module.ts
+++ apps/nx12/src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuillModule } from 'ngx-quill';
 
+@Component({ selector: 'app-other-component', template: '' })
+export class OtherComponent {}
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, QuillModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
